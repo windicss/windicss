619a3dbbad60f30794416edb1220084627cabd25
Anthony Fu <anthonyfu117@hotmail.com>
2021-02-18 19:38:09 +0800

    test: color diff, via `toEqualDiff` (#63)

--- test/extend.d.ts
+++ test/extend.d.ts
@@ -1,1 +1,11 @@
-declare function expectToMatchSnapshot(value: unknown, name: string, path:string): void
+declare function expectToMatchSnapshot(
+  value: unknown,
+  name: string,
+  path: string
+): void;
+
+declare namespace jasmine {
+  interface Matchers<T> {
+    toEqualDiff(expected:string, reasons?:string)
+  }
+}
