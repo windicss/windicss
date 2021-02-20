dc2f9ac9cbe48172f917203b861b633a2d31abda
Anthony Fu <anthonyfu117@hotmail.com>
2021-02-20 16:33:08 +0800

    fix: improve types for submodules

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,28 @@
-import fs from "fs";
-import path from "path";
-import replace from "@rollup/plugin-replace";
-import resolve from "@rollup/plugin-node-resolve";
-import sucrase from "@rollup/plugin-sucrase";
-import typescript from "@rollup/plugin-typescript";
+import fs from 'fs';
+import path from 'path';
+import replace from '@rollup/plugin-replace';
+import resolve from '@rollup/plugin-node-resolve';
+import sucrase from '@rollup/plugin-sucrase';
+import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import pkg from "./package.json";
+import pkg from './package.json';
 
-const output_dir = "./dist";
+const output_dir = './dist';
 
-const prod = process.env.NODE_ENV === "production";
+const prod = process.env.NODE_ENV === 'production';
 
 const ts_plugin = prod
   ? typescript({
-      target: "es5",
-      include: "src/**",
-      outDir: output_dir,
-      typescript: require("typescript"),
-    })
+    target: 'es5',
+    include: 'src/**',
+    outDir: output_dir,
+    typescript: require('typescript'),
+  })
   : sucrase({
-      exclude: ['node_modules/**'],
-      transforms: ["typescript"],
-    });
+    exclude: ['node_modules/**'],
+    transforms: ['typescript'],
+  });
 
 const dump = (file) => path.join(output_dir, file);
 
@@ -36,7 +36,7 @@ const rmdir = (dir) => {
     if (files.length > 0) {
       files.forEach((file) => {
         if (fs.statSync(path.join(dir, file)).isDirectory()) {
-          rmdir(dir + "/" + file);
+          rmdir(dir + '/' + file);
         } else {
           fs.unlinkSync(path.join(dir, file));
         }
@@ -48,14 +48,6 @@ const rmdir = (dir) => {
 const mkdir = (dir) =>
   !(fs.existsSync(dir) && fs.statSync(dir).isDirectory()) && fs.mkdirSync(dir);
 
-const types = (dest = "index.d.ts", src = "../types/index", module = "*") => {
-  return {
-    writeBundle() {
-      fs.writeFileSync(dump(dest), `export ${module} from "${src}";`);
-    },
-  };
-};
-
 const pack = (dir, mjs = true) => {
   return {
     writeBundle() {
@@ -63,12 +55,12 @@ const pack = (dir, mjs = true) => {
         `${dump(dir)}/package.json`,
         JSON.stringify(
           {
-            main: "./index.js",
-            module: "./index.mjs",
-            types: "./index.d.ts",
+            main: './index.js',
+            module: './index.mjs',
+            types: './index.d.ts',
           },
           null,
-          "  "
+          '  '
         )
       );
     },
@@ -78,118 +70,115 @@ const pack = (dir, mjs = true) => {
 export default [
   // main
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
-        file: dump("index.js"),
-        format: "cjs",
-        exports: "default",
-        paths: (id) => `./${path.relative("./src", id)}/index.js`,
+        file: dump('index.js'),
+        format: 'cjs',
+        exports: 'default',
+        paths: (id) => `./${path.relative('./src', id)}/index.js`,
       },
       {
-        file: dump("index.mjs"),
-        format: "esm",
-        paths: (id) => `./${path.relative("./src", id)}/index.mjs`,
+        file: dump('index.mjs'),
+        format: 'esm',
+        paths: (id) => `./${path.relative('./src', id)}/index.mjs`,
       },
     ],
-    external: (id) => id.startsWith("./"),
+    external: (id) => id.startsWith('./'),
     plugins: [
       ts_plugin,
       rmdir(output_dir),
       mkdir(output_dir),
-      copy(["package.json", "README.md", "LICENSE"]),
-      types("index.d.ts", "./types/lib", "{ Processor as default }"),
+      copy(['package.json', 'README.md', 'LICENSE']),
     ],
   },
 
   // colors
   {
-    input: "src/colors.ts",
+    input: 'src/colors.ts',
     output: [
       {
-        file: dump("colors.js"),
-        format: "cjs",
-        exports: "default",
-        paths: (id) => `./${path.relative("./src", id)}/index.js`,
+        file: dump('colors.js'),
+        format: 'cjs',
+        exports: 'default',
+        paths: (id) => `./${path.relative('./src', id)}/index.js`,
       },
       {
-        file: dump("colors.mjs"),
-        format: "esm",
-        paths: (id) => `./${path.relative("./src", id)}/index.mjs`,
+        file: dump('colors.mjs'),
+        format: 'esm',
+        paths: (id) => `./${path.relative('./src', id)}/index.mjs`,
       },
     ],
-    external: (id) => id.startsWith("./"),
+    external: (id) => id.startsWith('./'),
     plugins: [
       ts_plugin,
-      types("colors.d.ts", "./types/config", "{ colors as default }"),
     ],
   },
 
   // defaultConfig
   {
-    input: "src/defaultConfig.ts",
+    input: 'src/defaultConfig.ts',
     output: [
       {
-        file: dump("defaultConfig.js"),
-        format: "cjs",
-        exports: "default",
-        paths: (id) => `./${path.relative("./src", id)}/index.js`,
+        file: dump('defaultConfig.js'),
+        format: 'cjs',
+        exports: 'default',
+        paths: (id) => `./${path.relative('./src', id)}/index.js`,
       },
       {
-        file: dump("defaultConfig.mjs"),
-        format: "esm",
-        paths: (id) => `./${path.relative("./src", id)}/index.mjs`,
+        file: dump('defaultConfig.mjs'),
+        format: 'esm',
+        paths: (id) => `./${path.relative('./src', id)}/index.mjs`,
       },
     ],
-    external: (id) => id.startsWith("./"),
-    plugins: [ts_plugin, types("defaultConfig.d.ts", "./types/defaultConfig", "{ default }")],
+    external: (id) => id.startsWith('./'),
+    plugins: [ts_plugin],
   },
 
   // defaultTheme
   {
-    input: "src/defaultTheme.ts",
+    input: 'src/defaultTheme.ts',
     output: [
       {
-        file: dump("defaultTheme.js"),
-        format: "cjs",
-        exports: "default",
-        paths: (id) => `./${path.relative("./src", id)}/index.js`,
+        file: dump('defaultTheme.js'),
+        format: 'cjs',
+        exports: 'default',
+        paths: (id) => `./${path.relative('./src', id)}/index.js`,
       },
       {
-        file: dump("defaultTheme.mjs"),
-        format: "esm",
-        paths: (id) => `./${path.relative("./src", id)}/index.mjs`,
+        file: dump('defaultTheme.mjs'),
+        format: 'esm',
+        paths: (id) => `./${path.relative('./src', id)}/index.mjs`,
       },
     ],
-    external: (id) => id.startsWith("./"),
-    plugins: [ts_plugin, types("defaultTheme.d.ts", "./types/defaultTheme", "{ default }")],
+    external: (id) => id.startsWith('./'),
+    plugins: [ts_plugin],
   },
 
   // plugin
   {
-    input: `src/plugin/index.ts`,
+    input: 'src/plugin/index.ts',
     output: [
       {
-        file: dump(`plugin/index.js`),
-        exports: "default",
-        format: "cjs",
+        file: dump('plugin/index.js'),
+        exports: 'default',
+        format: 'cjs',
       },
       {
-        file: dump(`plugin/index.mjs`),
-        format: "esm",
+        file: dump('plugin/index.mjs'),
+        format: 'esm',
       },
     ],
     plugins: [
       ts_plugin,
       resolve(),
-      types(`plugin/index.d.ts`, `../types/plugin/index`, "{ default }"),
-      pack("plugin"),
+      pack('plugin'),
     ],
   },
 
   // plugin deep
   ...fs
-    .readdirSync("src/plugin")
+    .readdirSync('src/plugin')
     .filter(
       (dir) => fs.statSync(`src/plugin/${dir}`).isDirectory()
     )
@@ -198,9 +187,9 @@ export default [
       output: [
         {
           file: dump(`plugin/${dir}/index.js`),
-          exports: "default",
-          format: "cjs",
-        }
+          exports: 'default',
+          format: 'cjs',
+        },
       ],
       plugins: [
         ts_plugin,
@@ -211,15 +200,15 @@ export default [
 
   // cli
   {
-    input: "src/cli/index.ts",
+    input: 'src/cli/index.ts',
     output: [
       {
-        file: dump("cli/index.js"),
-        banner: "#!/usr/bin/env node",
-        format: "cjs",
+        file: dump('cli/index.js'),
+        banner: '#!/usr/bin/env node',
+        format: 'cjs',
         paths: (id) =>
           id.match(/\/src\/(lib|utils|plugin|config|colors)/) &&
-          `../${path.dirname(path.relative("./src", id))}/index.js`,
+          `../${path.dirname(path.relative('./src', id))}/index.js`,
       },
     ],
     external: (id) =>
@@ -231,24 +220,24 @@ export default [
       }),
       ts_plugin,
       resolve(),
-      commonjs()
+      commonjs(),
     ],
   },
 
   // utils
   ...fs
-    .readdirSync("src/")
-    .filter((dir) => ["config", "lib", "utils"].includes(dir) && fs.statSync(`src/${dir}`).isDirectory())
+    .readdirSync('src/')
+    .filter((dir) => ['config', 'lib', 'utils'].includes(dir) && fs.statSync(`src/${dir}`).isDirectory())
     .map((dir) => ({
       input: `src/${dir}/index.ts`,
       output: [
         {
           file: dump(`${dir}/index.js`),
-          format: "cjs",
+          format: 'cjs',
         },
         {
           file: dump(`${dir}/index.mjs`),
-          format: "esm",
+          format: 'esm',
         },
       ],
       plugins: [
@@ -256,28 +245,27 @@ export default [
         json(),
         resolve(),
         commonjs(),
-        types(`${dir}/index.d.ts`, `../types/${dir}/index`),
         pack(dir),
       ],
     })),
 
   // utils deep
   ...fs
-    .readdirSync("src/utils")
+    .readdirSync('src/utils')
     .filter(
       (dir) =>
-        dir !== "algorithm" && fs.statSync(`src/utils/${dir}`).isDirectory()
+        dir !== 'algorithm' && fs.statSync(`src/utils/${dir}`).isDirectory()
     )
     .map((dir) => ({
       input: `src/utils/${dir}/index.ts`,
       output: [
         {
           file: dump(`utils/${dir}/index.js`),
-          format: "cjs",
+          format: 'cjs',
         },
         {
           file: dump(`utils/${dir}/index.mjs`),
-          format: "esm",
+          format: 'esm',
         },
       ],
       plugins: [
@@ -285,7 +273,6 @@ export default [
         json(),
         resolve(),
         commonjs(),
-        types(`utils/${dir}/index.d.ts`, `../../types/utils/${dir}/index`),
         pack(`utils/${dir}`),
       ],
     })),
