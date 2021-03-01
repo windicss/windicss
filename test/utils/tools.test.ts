16e170dab3173e914a3d9eba332f12c6316b2414
voorjaar <felitas@zoho.com>
2021-03-01 09:44:47 +0800

    feat: add windi template literal

--- test/utils/tools.test.ts
+++ test/utils/tools.test.ts
@@ -21,6 +21,7 @@ import {
   flatColors,
   transform,
   searchPropEnd,
+  windi,
 } from '../../src/utils';
 
 import { Property, Style } from '../../src/utils/style';
@@ -236,5 +237,11 @@ describe('Tools', () => {
   it('search property end', () => {
     expect(searchPropEnd('font-family: "iconfont";')).toEqual(23);
     expect(searchPropEnd(String.raw`src: url('data:application/x-font-woff2;charset=utf-8;base64,d09GMgABAAAAAAXEAAsAAAAACy') format('woff2');`)).toEqual(105);
+  });
+
+  it('template literal', () => {
+    const color = 'red-500';
+    const style = windi`bg-gray-100 text-${color} rounded`;
+    expect(style).toEqual('bg-gray-100 text-red-500 rounded');
   });
 });
