d070e1e9ef81932ae0231cc29afaa6b4a2221974
voorjaar <felitas@zoho.com>
2021-03-03 11:55:39 +0800

    fix: update snapshots

--- test/processor/preflight.test.ts
+++ test/processor/preflight.test.ts
@@ -6,15 +6,13 @@ const processor = new Processor();
 
 describe('Preflight', () => {
   it('some tags', () => {
-    expect(processor.preflight(html).build()).toEqual(readFileSync('./test/assets/examplePreflight.css').toString());
+    expect(processor.preflight(html).build()).toMatchSnapshot('tags');
 
     expect(processor.preflight(html, true, false).build()).toMatchSnapshot('css');
     expect(processor.preflight(html, false, false, false).build()).toEqual('');
   });
 
   it('all tags', () => {
-    expect(processor.preflight().build()).toEqual(
-      readFileSync('./test/assets/preflight.css').toString()
-    );
+    expect(processor.preflight().build()).toMatchSnapshot('tags');
   });
 });
