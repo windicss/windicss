e73c38914be7b696c0ffe1759ef0ba30db0c3594
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-05 19:22:34 +0800

    test: add failed tests for #219

--- test/interface/plugin.test.ts
+++ test/interface/plugin.test.ts
@@ -1,15 +1,16 @@
-import plugin from '../../dist/plugin';
-import type { PluginUtils } from '../../dist/types/interfaces';
+import Processor from "../../src";
+import plugin from "../../src/plugin";
+import type { PluginUtils, PluginFunction } from "../../src/interfaces";
 
-describe('plugin interface test', () => {
-  it('import', () => {
-    plugin(function({ addUtilities }) {
+describe("plugin interface test", () => {
+  it("import", () => {
+    plugin(function ({ addUtilities }) {
       const newUtilities = {
-        '.skew-10deg': {
-          transform: 'skewY(-10deg)',
+        ".skew-10deg": {
+          transform: "skewY(-10deg)",
         },
-        '.skew-15deg': {
-          transform: 'skewY(-15deg)',
+        ".skew-15deg": {
+          transform: "skewY(-15deg)",
         },
       };
 
@@ -17,20 +18,43 @@ describe('plugin interface test', () => {
     });
   });
 
-  it('require', () => {
-    const plugin = require('../../dist/plugin');
-    plugin(function({ addUtilities }: PluginUtils) {
+  it("require", () => {
+    const plugin = require("../../dist/plugin");
+    plugin(function ({ addUtilities }: PluginUtils) {
       const newUtilities = {
-        '.skew-10deg': {
-          transform: 'skewY(-10deg)',
+        ".skew-10deg": {
+          transform: "skewY(-10deg)",
         },
-        '.skew-15deg': {
-          transform: 'skewY(-15deg)',
+        ".skew-15deg": {
+          transform: "skewY(-15deg)",
         },
       };
 
       addUtilities(newUtilities);
     });
     expect(plugin.withOptions).toBeDefined();
+  });
+
+  it("addComponents order", () => {
+    const plugin: PluginFunction = (utils) => {
+      utils.addComponents({
+        "[data-theme=dark]": {
+          "-P": "259 94% 61%",
+        },
+        "@media (prefers-color-scheme:dark)": {
+          ":root": {
+            "-P": "259 94% 61%",
+          },
+        },
+        "[data-theme=light]": {
+          "-P": "259 94% 51%",
+        },
+      });
+    };
+    const processor = new Processor({
+      plugins: [plugin],
+    });
+
+    expect(processor.preflight(' ', false, false, true).build()).toMatchSnapshot('css')
   });
 });
