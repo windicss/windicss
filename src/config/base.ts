8583d0f4821d428bb1385fdbc7bbcb95b308ff31
voorjaar <felitas@zoho.com>
2021-05-19 23:23:22 +0800

    perf: move Default to top

--- src/config/base.ts
+++ src/config/base.ts
@@ -132,9 +132,9 @@ export const baseConfig: Config = {
       contain: 'contain',
     },
     blur: {
+      DEFAULT: '8px',
       0: '0',
       sm: '4px',
-      DEFAULT: '8px',
       md: '12px',
       lg: '16px',
       xl: '24px',
@@ -142,14 +142,14 @@ export const baseConfig: Config = {
       '3xl': '64px',
     },
     borderColor: (theme) => ({
-      ...(theme('colors') ?? {}),
       DEFAULT: theme('colors.gray.200', 'currentColor'),
+      ...(theme('colors') ?? {}),
     }),
     borderOpacity: (theme) => theme('opacity'),
     borderRadius: {
+      DEFAULT: '0.25rem',
       none: '0px',
       sm: '0.125rem',
-      DEFAULT: '0.25rem',
       md: '0.375rem',
       lg: '0.5rem',
       xl: '0.75rem',
@@ -168,8 +168,8 @@ export const baseConfig: Config = {
       // int >=0 -> int px
     },
     boxShadow: {
-      sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
       DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+      sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
       md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
       lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
       xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
@@ -220,8 +220,8 @@ export const baseConfig: Config = {
     divideOpacity: (theme) => theme('borderOpacity'),
     divideWidth: (theme) => theme('borderWidth'),
     dropShadow: {
-      sm: '0 1px 1px rgba(0,0,0,0.05)',
       DEFAULT: ['0 1px 2px rgba(0, 0, 0, 0.1)', '0 1px 1px rgba(0, 0, 0, 0.06)'],
+      sm: '0 1px 1px rgba(0,0,0,0.05)',
       md: ['0 4px 3px rgba(0, 0, 0, 0.07)', '0 2px 2px rgba(0, 0, 0, 0.06)'],
       lg: ['0 10px 8px rgba(0, 0, 0, 0.04)', '0 4px 3px rgba(0, 0, 0, 0.1)'],
       xl: ['0 20px 13px rgba(0, 0, 0, 0.03)', '0 8px 5px rgba(0, 0, 0, 0.08)'],
@@ -239,12 +239,12 @@ export const baseConfig: Config = {
       none: 'none',
     },
     flexGrow: {
-      0: '0',
       DEFAULT: '1',
+      0: '0',
     },
     flexShrink: {
-      0: '0',
       DEFAULT: '1',
+      0: '0',
     },
     fontFamily: {
       sans: [
@@ -313,8 +313,8 @@ export const baseConfig: Config = {
     gap: (theme) => theme('spacing'),
     gradientColorStops: (theme) => theme('colors'),
     grayscale: {
-      0: '0',
       DEFAULT: '100%',
+      0: '0',
     },
     gridAutoColumns: {
       auto: 'auto',
@@ -511,8 +511,8 @@ export const baseConfig: Config = {
       // ...negative
     }),
     invert: {
-      0: '0',
       DEFAULT: '100%',
+      0: '0',
     },
     keyframes: {
       spin: {
@@ -793,8 +793,8 @@ export const baseConfig: Config = {
       // int >=0 -> int/100
     },
     sepia: {
-      0: '0',
       DEFAULT: '100%',
+      0: '0',
     },
     skew: {
       '-12': '-12deg',
@@ -880,8 +880,8 @@ export const baseConfig: Config = {
       '3xl': '4rem',
     },
     textStrokeColor: (theme) => ({
-      ...(theme('colors') ?? {}),
       DEFAULT: theme('colors.gray.200', 'currentColor'),
+      ...(theme('colors') ?? {}),
     }),
     textStrokeWidth: {
       DEFAULT: 'medium',
@@ -925,10 +925,9 @@ export const baseConfig: Config = {
       // int >=0 -> int ms
     },
     transitionProperty: {
+      DEFAULT: 'background-color, border-color, color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter',
       none: 'none',
       all: 'all',
-      DEFAULT:
-        'background-color, border-color, color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter',
       colors: 'background-color, border-color, color, fill, stroke',
       opacity: 'opacity',
       shadow: 'box-shadow',
