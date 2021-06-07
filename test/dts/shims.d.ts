fe21be722cddfca2730ed193d981b70bf3c4cf90
voorjaar <felitas@zoho.com>
2021-06-07 20:06:18 +0800

    fix: 'T' is defined but never used

--- test/dts/shims.d.ts
+++ test/dts/shims.d.ts
@@ -2,5 +2,6 @@
 import { AttributifyAttributes } from '../../src/jsx';
 
 declare module 'react' {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface HTMLAttributes<T> extends AttributifyAttributes {}
 }
