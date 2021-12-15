8c39b4346a3cdb05002818f4969c8f52894f3b79
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-16 04:05:31 +0800

    Update test/processor/resolve.test.ts

--- test/processor/resolve.test.ts
+++ test/processor/resolve.test.ts
@@ -44,8 +44,8 @@ describe('Resolve Tests', () => {
   });
 
   it('resolve static utilities', () => {
-    expect(Object.keys(processor.resolveStaticUtilities(false)).length).toEqual(313);
-    expect(Object.keys(processor.resolveStaticUtilities(true)).length).toEqual(322);
+    expect(Object.keys(processor.resolveStaticUtilities(false)).length).toEqual(320);
+    expect(Object.keys(processor.resolveStaticUtilities(true)).length).toEqual(329);
   });
 
   it('resolve dynamic utilities', () => {
