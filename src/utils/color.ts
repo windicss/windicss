ca75afd406ca0cc4c84f5b7b71e005f210d0c196
Satyrn <pastawho@gmail.com>
2022-02-24 11:48:12 -0800

    revert one change

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -2,25 +2,36 @@ import colorString from 'color-string';
 import type { Color } from 'color-string';
 
 export function hsl2rgb(h: number, s: number, l: number): [number, number, number] {
-  if (h >= 360) h %= 360;
+  s = s / 100,
+  l = l / 100;
+  if (h >= 360)
+    h %= 360;
 
-  const c = ((1 - Math.abs(2 * (l / 100) - 1)) * (s / 100)) * 255;
-  const x = (c * (1 - Math.abs((h / 60) % 2 - 1))) * 255;
-  const m = (l - c/2) * 255;
+  const c = (1 - Math.abs(2 * l - 1)) * s;
+  const x = c * (1 - Math.abs((h / 60) % 2 - 1));
+  const m = l - c/2;
+  let r = 0;
+  let g = 0;
+  let b = 0;
 
   if (0 <= h && h < 60) {
-    return [Math.round(c + m), Math.round(x + m), Math.round(m)];
+    r = c; g = x; b = 0;
   } else if (60 <= h && h < 120) {
-    return [Math.round(x + m), Math.round(c + m), Math.round(m)];
+    r = x; g = c; b = 0;
   } else if (120 <= h && h < 180) {
-    return [Math.round(m), Math.round(c + m), Math.round(x + m)];
+    r = 0; g = c; b = x;
   } else if (180 <= h && h < 240) {
-    return [Math.round(m), Math.round(x + m), Math.round(c + m)];
+    r = 0; g = x; b = c;
   } else if (240 <= h && h < 300) {
-    return [Math.round(x + m), Math.round(m), Math.round(c + m)];
+    r = x; g = 0; b = c;
   } else if (300 <= h && h < 360) {
-    return [Math.round(c + m), Math.round(m), Math.round(x + m)];
+    r = c; g = 0; b = x;
   }
+
+  r = Math.round((r + m) * 255);
+  g = Math.round((g + m) * 255);
+  b = Math.round((b + m) * 255);
+  return [r, g, b];
 }
 
 export function hwb2rgb(h: number, w: number, b: number): [number, number, number] {
